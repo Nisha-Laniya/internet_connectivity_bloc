3d1661c687c2ac1ab5c5acb6503ce08e47c3bb40
NishaLaniya <nishal@zignuts.com>
2023-03-24 14:18:53 +0530

    snack bar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,16 +11,26 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: BlocBuilder<InternetBloc,InternetState>(
-            builder: (context, state) {
-
-              //if(state == )
-              //== to check value
-              //is to check data type
-              //int a = 2;
-              //if(a == 2)
-              //if(a is int)
-
+          child: BlocConsumer<InternetBloc,InternetState> (
+            listener: (context,state) {
+              if(state is InternetGainedState) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                      content: Text('Internet connected'),
+                      backgroundColor: Colors.green,
+                  ),
+                );
+              }
+              else if(state is InternetLostState) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Internet not connected'),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+              }
+            },
+            builder: (context,state) {
               if(state is InternetGainedState) {
                 return Text('Connected');
               } else if (state is InternetLostState) {
@@ -30,6 +40,28 @@ class HomePage extends StatelessWidget {
               }
             },
           )
+
+
+            //BlocBuilder
+          // BlocBuilder<InternetBloc,InternetState>(
+          //   builder: (context, state) {
+          //
+          //     //if(state == )
+          //     //== to check value
+          //     //is to check data type
+          //     //int a = 2;
+          //     //if(a == 2)
+          //     //if(a is int)
+          //
+          //     if(state is InternetGainedState) {
+          //       return Text('Connected');
+          //     } else if (state is InternetLostState) {
+          //       return Text('Not Connected');
+          //     } else {
+          //       return Text('Loading..');
+          //     }
+          //   },
+          // )
         )
       ),
     );
